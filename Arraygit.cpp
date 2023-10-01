a01f40727485f8d15a38e9c81943add5493dabb5
Saksham-1607 <sakshamdubey022@gmail.com>
2023-10-01 11:24:42 +0530

    Array  Git

---
+++ Arraygit.cpp
@@ -0,0 +1,14 @@
+//Initializing and Printing the array
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {5,4,8,3,6};
+    int i;
+    cout << "Printing the array" <<endl;
+    for(i=0; i<=4 ; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
